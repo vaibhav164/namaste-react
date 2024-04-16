2221830a5860b18922a9fb3fc1d82e6902e19aaa
vaibhav164 <vai1641996@gmail.com>
2024-04-17 04:14:43 +0530

    Learned about the Return type or constant value saved on return of React.createElement()

--- App.js
+++ App.js
@@ -8,5 +8,24 @@ const heading = React.createElement(
  * attributes as i had passed and we can check it in Element of Browser
  * as this <h1 id="heading" name="vaibhavReact">EHading from React</h1>
  */
+console.log("heading______", heading);
+/****
+ *
+ * if we console the heading or return value of React.createElement
+ * we get an Object not an h1 tag and that object will be used to create
+ * h1 tag in dom an object looks as below
+ {
+  $$typeof: Symbol(react.element)
+  key: null
+  props: {id: 'heading', name: 'vaibhavReact', children: 'EHading from React'}
+   ref: null
+   type: "h1"
+ _owner:null
+_store: {validated: false}
+_self: null
+_source: null
+[[Prototype]]: Object
+}
+ */
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
