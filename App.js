9c458f36983c6ee974b068e4352705c4ce98616c
vaibhav164 <vai1641996@gmail.com>
2024-04-17 02:47:14 +0530

    Learned about the sencond parameter of React.createElement

--- App.js
+++ App.js
@@ -1,3 +1,12 @@
-const heading = React.createElement("h1", {}, "EHading from React");
+const heading = React.createElement(
+  "h1",
+  { id: "heading", name: "vaibhavReact" },
+  "EHading from React"
+);
+/****
+ * in above React,createElement the second parameter is {} and it takes
+ * attributes as i had passed and we can check it in Element of Browser
+ * as this <h1 id="heading" name="vaibhavReact">EHading from React</h1>
+ */
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
